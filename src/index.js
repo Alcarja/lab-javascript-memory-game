0bd2ec9963f4bde9a18c85b91a6f4e7bf4703dd9
Alcaraz <jaime.mediano@gmail.com>
2023-06-21 18:42:11 +0200

    Got exactly 1 line of code correct in this lab. Copied the solution and tried to make sense of it.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+//SOLUTION
+
 const cards = [
   { name: 'aquaman', img: 'aquaman.jpg' },
   { name: 'batman', img: 'batman.jpg' },
@@ -27,6 +29,10 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+function toggle(element, classes) {
+  classes.forEach(className => element.classList.toggle(className));
+}
+
 window.addEventListener('load', (event) => {
   let html = '';
   memoryGame.cards.forEach((pic) => {
@@ -41,11 +47,60 @@ window.addEventListener('load', (event) => {
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
+  function toggle(element, classes) {
+    classes.forEach(className => element.classList.toggle(className));
+  }
+
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+     const clicked = document.getElementById("pairs_clicked");
+     const guessed = document.getElementById("pairs_guessed");
+
+     console.log("Card clicked: ", card);
+     toggle(card.children[0], ["back", "front"]); //We use this to toggle between the front and the back of the card
+     toggle(card.children[1], ["back", "front"]);
+
+     memoryGame.pickedCards.push(card); //When we click a card we push it into our pickedCards array
+
+     if (memoryGame.pickedCards.length === 2) {
+       const firstInPair = memoryGame.pickedCards[0];
+       const secondInPair = memoryGame.pickedCards[1];
+       const cardName1 = firstInPair.getAttribute("data-card-name");
+       const cardName2 = secondInPair.getAttribute("data-card-name");
+       //Si la longitud del array es 2 hacemos que la primera se llame 'firstInPair' y la segunda se llame 'secondInPair, y le damos el atributo 'data-card-name'
+
+       if (memoryGame.checkIfPair(cardName1, cardName2)) {
+         firstInPair.children[1].classList.add("blocked");
+         secondInPair.children[1].classList.add("blocked");
+         memoryGame.pickedCards = [];
+         //Si nuestra función 'checkIfPair' con los dos parámetros es verdad (funciona) entonces hacemos que la clase de cardName1 (firstInPair) y cardName2 (secondInPair) sea 'blocked', y vaciamos el array de picked cards para poder empezar de 0 con una nueva pareja
+
+
+        if (memoryGame.checkIfFinished()) {
+          document.querySelector("#memory-board").innerHTML = "";
+          let h1 = document.createElement("h1");
+          h1.style.color = "pink";
+          h1.innerHTML = "YOU WON!!!";
+          document.querySelector("#memory-board").appendChild(h1);
+        }
+
+       } else {
+         setTimeout(() => {
+           toggle(firstInPair.children[0], ["back", "front"]);
+           toggle(firstInPair.children[1], ["back", "front"]);
+           toggle(secondInPair.children[0], ["back", "front"]);
+           toggle(secondInPair.children[1], ["back", "front"]);
+         }, 1000);
+         memoryGame.pickedCards = [];
+       }
+       clicked.innerHTML = memoryGame.pairsClicked;
+       guessed.innerHTML = memoryGame.pairsGuessed;
+     }
+
     });
   });
+  
 });
+
--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,39 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
+    if(!this.cards) {
+      return undefined;
+    }
+    for (let i = this.cards.length - 1; i > 0; i--) {
+     const j = Math.floor(Math.random() * (i + 1));
+     [this.cards[i]], this.cards[j] = [this.cards[j], this.cards[i]];
+    }
     // ... write your code here
   }
 
+  
+
+
   checkIfPair(card1, card2) {
+    this.pairsClicked += 1;
+    if(card1 === card2) {
+      this.pairsGuessed +=1;
+      return true;
+    } else {return false}
     // ... write your code here
   }
 
   checkIfFinished() {
+    if(this.pairsGuessed === this.cards.length / 2) {
+       return true;
+    } else {return false}
     // ... write your code here
   }
 }
